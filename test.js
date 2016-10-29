1c276bb628cb41b511cab55d38fb71f522d424a9
RaulJimMat <rauljimmat@gmail.com>
2016-10-29 12:17:40 -0500

    Primer commit

---
+++ test.js
@@ -0,0 +1,13 @@
+function findPrice(){
+
+	price = 1200;
+}
+findPrice();
+
+function declareVariable(){
+	if(true){
+		let x = "Declared in  a block of code";
+	}
+	console.log("Value of x: " + x);
+};
+declareVariable();
