466e3d1f442fb10c9f7b4ac78a979119dd9d2a2d
RaulJimMat <rauljimmat@gmail.com>
2016-11-05 12:25:01 -0600

    Server.js added

---
+++ myPersonalWebPage/server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('GET Hello World!');
+});
+
+app.post('/', function (req, res) {
+  res.send('POST Hello World!');
+});
+
+app.post('/mail/process' (req,res) =>{
+	res.send('POST Hello World!');
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
